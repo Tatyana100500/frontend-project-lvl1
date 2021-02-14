ec0a741bb6050e6e2288f236f235c23adf72c569
Tatiana Chernishova <chernishovate@mail.ru>
2021-02-14 16:10:12 +0300

    fix 2 files

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
-import sayHello from '../src/cli';
+import sayHello from '../src/cli'
 
-console.log('Welcome to the Brain Games!');
-const name = sayHello();
-console.log(name);
+console.log('Welcome to the Brain Games!')
+const name = sayHello()
+console.log(name)
