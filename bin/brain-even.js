c6b5b464e59eb909a5f37bb7721028a9282a2d5c
Tatiana Chernishova <chernishovate@mail.ru>
2021-02-19 15:45:25 +0300

    step 5

---
+++ bin/brain-even.js
@@ -0,0 +1,42 @@
+//import sayHello from '../src/cli.js'
+import readlineSync from 'readline-sync'
+
+console.log('Welcome to the Brain Games!')
+const name = readlineSync.question('May I have your name?')
+console.log(`Hello, ${name}!`)
+console.log('Answer "yes" if the number is even, otherwise answer "no".')
+const getRandomInt = (max) => {
+    return Math.floor(Math.random() * Math.floor(max))
+}
+  
+const answer = () => {
+    const num = getRandomInt(100)
+    let correctAnswer = ''
+    if (num %2 === 0) {
+        correctAnswer = 'yes'
+    } else {
+        correctAnswer = 'no'
+    }
+    const a = readlineSync.question(`Question: ${num}`)
+    if (a === correctAnswer) {
+      return 'Correct!'
+    } else {
+      return `${a} is wrong answer ;(. Correct answer was ${correctAnswer}.`
+    }
+}
+let count = 0
+const winOrNo = (answer, count) => {
+    const str = answer()
+    count += 1
+    if (str !== 'Correct!') {
+      return `${str} 
+Let's try again, ${name}!`
+    } else {
+      if (count === 3) {
+        return `Congratulations, ${name}!`
+      } else {
+        return winOrNo(answer, count)
+      }
+    }
+}
+console.log(winOrNo(answer, count))
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,4 @@
-import sayHello from '../src/cli'
+import sayHello from '../src/cli.js'
 
 console.log('Welcome to the Brain Games!')
 const name = sayHello()
