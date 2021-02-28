ac87bc2efa3d33b09fc16a3d698e8ab5717137f2
Tatiana Chernishova <chernishovate@mail.ru>
2021-02-28 16:15:17 +0300

    step7

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -7,19 +7,21 @@ console.log('Find the greatest common divisor of given numbers.')
 const getRandomInt = (max) => {
   return Math.floor(Math.random() * Math.floor(max))
 }
-Math.gcd = function() {
-    if (arguments.length == 2) {
-        if (arguments[1] == 0)
-            return arguments[0];
-        else
-            return Math.gcd(arguments[1], arguments[0] % arguments[1]);
-    } else if (arguments.length > 2) {
-        var result = Math.gcd(arguments[0], arguments[1]);
-        for (var i = 2; i < arguments.length; i++)
-            result = Math.gcd(result, arguments[i]);
-        return result;
+Math.gcd = function () {
+  if (arguments.length === 2) {
+    if (arguments[1] === 0)
+      return arguments[0]
+    else {
+      return Math.gcd(arguments[1], arguments[0] % arguments[1])
     }
-};
+  } else if (arguments.length > 2) {
+    let result = Math.gcd(arguments[0], arguments[1])
+    for (var i = 2; i < arguments.length; i++) {
+      result = Math.gcd(result, arguments[i])
+    }
+    return result
+  }
+}
 const answer = () => {
   const num1 = getRandomInt(100)
   console.log(num1)
