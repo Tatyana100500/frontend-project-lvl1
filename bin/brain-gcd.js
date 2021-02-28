31f5e837cfb006c660840e8640dda23a12bd100e
Tatiana Chernishova <chernishovate@mail.ru>
2021-02-28 16:19:54 +0300

    step 7

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -9,14 +9,15 @@ const getRandomInt = (max) => {
 }
 Math.gcd = function () {
   if (arguments.length === 2) {
-    if (arguments[1] === 0)
+    if (arguments[1] === 0) {
       return arguments[0]
+    }
     else {
       return Math.gcd(arguments[1], arguments[0] % arguments[1])
     }
   } else if (arguments.length > 2) {
     let result = Math.gcd(arguments[0], arguments[1])
-    for (var i = 2; i < arguments.length; i++) {
+    for (let i = 2; i < arguments.length; i++) {
       result = Math.gcd(result, arguments[i])
     }
     return result
